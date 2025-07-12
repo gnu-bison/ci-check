7edcdf8fd8573d80d24566f1c299690f902b86c7
Bruno Haible <bruno@clisp.org>
2025-07-12 07:14:15 +0200

    Work on build-tarball.

--- build-tarball.sh
+++ build-tarball.sh
@@ -25,7 +25,7 @@ set -e
 
 # Fetch sources (uses package 'git').
 git clone --depth 1 https://git.savannah.gnu.org/git/"$package".git
-git submodule update --init submodules/autoconf
+(cd "$package" && git submodule update --init submodules/autoconf)
 git clone --depth 1 "${gnulib_url}"
 export GNULIB_SRCDIR=`pwd`/gnulib
 
